458069e1f4352dc1ebf0ea300f19c38e1dd4f3cf
Srini <adscapebillboards@gmail.com>
2025-10-06 16:38:53 +0530

    Auth

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(cors({
     const allowedOrigins = [
       "http://localhost:3000",
       "http://localhost:5173",
-      "http://127.0.0.1:3000",
+      "http://127.0.0.1:3000", 
       "http://127.0.0.1:5173",
       "https://your-frontend-domain.com",
        "https://adscape.co.in",
